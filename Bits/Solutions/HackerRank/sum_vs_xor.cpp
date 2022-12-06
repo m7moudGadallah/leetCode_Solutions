5ef9573e52d834a95e58be0b0638dbb4cd9b9397
m7moudGadallah <mahmoudgadallah876@gmail.com>
2022-12-07 00:20:47 +0200

    solution of problem  Sum vs XOR

--- Bits/Solutions/HackerRank/sum_vs_xor.cpp
+++ Bits/Solutions/HackerRank/sum_vs_xor.cpp
@@ -2,16 +2,6 @@
 
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-
-/*
- * Complete the 'sumXor' function below.
- *
- * The function is expected to return a LONG_INTEGER.
- * The function accepts LONG_INTEGER n as parameter.
- */
-
 long sumXor(long n) {
     long long cnt{1};
     

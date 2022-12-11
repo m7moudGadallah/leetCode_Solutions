63b8b65f8942657c65152a60e3e72f9cee980783
m7moudGadallah <mahmoudgadallah876@gmail.com>
2022-12-12 01:31:06 +0200

    solution of problem 56. Merge Intervals

--- Searching&Sorting/Solutions/65-merge_intervals.cpp
+++ Searching&Sorting/Solutions/65-merge_intervals.cpp
@@ -2,3 +2,33 @@
 
 using namespace std;
 
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+
+
+        vector<vector<int>> ans;
+
+        for (const auto &x : intervals)
+        {
+            if (ans.empty())
+                ans.emplace_back(x);
+            else
+            {
+                auto tmp = ans.back();
+
+                if (x[0] <= tmp[1])
+                {
+                    tmp[1] = max(tmp[1], x[1]);
+                    ans.pop_back();
+                    ans.emplace_back(tmp);
+                }
+                else
+                    ans.emplace_back(x);
+            }
+        }
+
+        return ans;
+    }
+};

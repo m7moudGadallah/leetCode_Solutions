392cb24f8a8cc0be63d0a6eef002aa0f9a06fc91
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-01-02 12:42:37 +0200

    solution of problem 520. Detect Capital || Beats 100%

---
+++ dailyStreak/2023/01.Jan/520-detect_capital.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = (int) word.size();
+        int cap_num{0};
+        
+        auto is_up = [&] (const char &c) {return c >= 'A' and c <= 'Z';};
+
+        for (const auto &ch : word)
+            cap_num += is_up(ch);
+        
+        if (is_up(word.front()) and cap_num == 1)
+            cap_num = 0;
+
+        return !(cap_num >= 1 and cap_num < n);
+    }
+};
--- dailyStreak/2023/01.Jan/README.md
+++ dailyStreak/2023/01.Jan/README.md
@@ -2,3 +2,4 @@
 |Day|Problem|Solution|topic|
 |---|-------|--------|-----|
 |1/1|[290. Word Pattern](https://leetcode.com/problems/word-pattern/)|[Link](./290-word_pattern.cpp)|`String` `Hash Table`|
+|2/1|[520. Detect Capital](https://leetcode.com/problems/detect-capital/)|[Link](./520-detect_capital.cpp)|`String`|

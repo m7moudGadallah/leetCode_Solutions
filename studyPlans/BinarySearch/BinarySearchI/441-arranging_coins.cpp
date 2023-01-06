1f7d70e5d52e914ccc3f55c13b29ff2e438f7608
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-01-06 17:46:45 +0200

    solution of problem 441. Arranging Coins

---
+++ studyPlans/BinarySearch/BinarySearchI/441-arranging_coins.cpp
@@ -0,0 +1,31 @@
+/**
+ * Author: Mahmoud Gadallah
+ * LeetCode Account: https://leetcode.com/m7moudGadallah/
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+private:
+    bool good(const int &rows, const int &n)
+    {
+        return (long long)rows * (rows + 1) / 2 <= n;
+    }
+public:
+    int arrangeCoins(int n) {
+        int l{0}, r{n}, mid{0}, ans{0};
+
+        while (l <= r)
+        {
+            mid = l + (r - l) / 2;
+
+            if (good(mid, n))
+                ans = mid, l = mid + 1;
+            else
+                r = mid - 1;
+        }
+
+        return ans;
+    }
+};

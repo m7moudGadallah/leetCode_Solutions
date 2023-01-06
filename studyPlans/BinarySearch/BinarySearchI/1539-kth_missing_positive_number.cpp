e8d23d2ce236d0ea483c6344f49d2b473b7939af
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-01-06 17:54:17 +0200

    solution of problem 1539. Kth Missing Positive Number

---
+++ studyPlans/BinarySearch/BinarySearchI/1539-kth_missing_positive_number.cpp
@@ -0,0 +1,36 @@
+/**
+ * Author: Mahmoud Gadallah
+ * LeetCode Account: https://leetcode.com/m7moudGadallah/
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+private:
+    inline int offest(const int &actual ,const int &correct)
+    {
+        return actual - correct;
+    }
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        
+        int n = (int) arr.size();
+        int l {0}, r{n - 1}, mid{0}, ans{-1};
+
+        while (l <= r)
+        {
+            mid = l + (r - l) / 2;
+        
+            if (mid == 0 or offest(arr[mid - 1], mid) < k)
+                ans = mid, l = mid + 1;
+            else
+                r = mid - 1;
+        }
+
+        if (ans == n - 1 and ans + k >= arr.back())
+            return ans + k + 1;
+
+        return ans + k;
+    }
+};

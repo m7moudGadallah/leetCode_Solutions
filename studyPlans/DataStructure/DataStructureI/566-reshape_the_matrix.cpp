4beb67d88945c06fcee94a3efdf63e7f760d5307
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-01-06 18:41:30 +0200

    solution of problem 566. Reshape the Matrix

---
+++ studyPlans/DataStructure/DataStructureI/566-reshape_the_matrix.cpp
@@ -0,0 +1,31 @@
+/**
+ * Author: Mahmoud Gadallah
+ * LeetCode Account: https://leetcode.com/m7moudGadallah/
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int n = mat.size(), m = mat[0].size();
+
+        if (n * m != r * c)
+            return mat;
+        
+        int i = 0, j = 0;
+        vector<vector<int>> ans(r, vector<int>(c));
+
+        for (auto &x : mat) {
+            for (auto &y : x) {
+                ans[i][j++] = y;
+
+                if (j >= c)
+                    ++i, j = 0;        
+            }
+        }
+
+        return ans;
+    }
+};

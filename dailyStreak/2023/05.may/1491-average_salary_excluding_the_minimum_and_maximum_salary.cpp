17a2c4c229dc67d9b8c7ecca3a7bcdc388e5b2b0
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-05-02 07:45:15 +0200

    Add solutoin of problem 1491. Average Salary Excluding the Minimum and Maximum Salary

---
+++ dailyStreak/2023/05.may/1491-average_salary_excluding_the_minimum_and_maximum_salary.cpp
@@ -0,0 +1,21 @@
+/**
+ *  @author Author: Mahmoud Gadallah
+ *  @link LeetCode Account: https://leetcode.com/m7moudGadallah/
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        int mx = INT_MIN, mn = INT_MAX;
+        double tot = 0;
+
+        for (auto &x : salary)
+            mx = max(mx, x), mn = min(mn, x), tot += x;
+        
+        return (tot - (mx+mn)) / (salary.size()-2);
+    }
+};
---
+++ dailyStreak/2023/05.may/README.md
@@ -0,0 +1,5 @@
+# Daily Streak May 2023
+
+|Day|Problem|Solution|topic|
+|---|-------|--------|-----|
+|1/5|[1491. Average Salary Excluding the Minimum and Maximum Salary](https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/)|[Link](./1491-average_salary_excluding_the_minimum_and_maximum_salary.cpp)|`Array` `Sorting`|
